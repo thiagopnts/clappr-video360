47c1a809239caa53625385a7b37fb7fff97e57ab
Mikhail Vorobyov <vorobyov.mikhail@gmail.com>
2017-12-21 17:34:00 +0300

    Prevent from scrolling whole page while panning with touchscreen

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ export default class Video360 extends ContainerPlugin {
     Mediator.on(`${this.options.playerId}:${Events.PLAYER_RESIZE}`, this.updateSize, this);
     let {height, width, autoplay} = container.options;
     container.playback.el.setAttribute('crossorigin', 'anonymous');
+    container.el.style.touchAction = "none";
+    container.el.addEventListener("touchmove", function(event) {
+      event.preventDefault();
+    }, false);
     this.viewer = new Video({height: isNaN(height) ? 300 : height, width: isNaN(width) ? 400 : width, container: this.container.el, source: this.container.playback.el});
     this.viewer.render();
   }
